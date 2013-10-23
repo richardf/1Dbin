7f7ec370732ee8cf7afb373b0a4f63ee85295f2d
Richard Fuchshuber <richardfuch@gmail.com>
2013-10-23 12:20:47 +0000

    read ORLibrary instances

--- binp.py
+++ binp.py
@@ -1,7 +1,47 @@
 
 class ORLibraryInstanceReader(object):
 
-	@classmethod
-	def _get_instance_definition(cls, line):
-		bin_capacity, number_of_itens, bins_in_best_sol = line.split()
-		return int(bin_capacity), int(number_of_itens), int(bins_in_best_sol)
+    @classmethod
+    def _get_instance_definition(cls, line):
+        """It returns the bin capacity, number of itens in instance and the number of bins used 
+        in the best known solution"""
+        bin_capacity, number_of_itens, bins_in_best_sol = line.split()
+        return int(bin_capacity), int(number_of_itens), int(bins_in_best_sol)
+
+    @classmethod
+    def _get_number_of_instances(cls, data):
+        return int(data[0])
+    
+    @classmethod
+    def _load_instance(cls, file):
+        pass
+    
+    @classmethod
+    def get_instances(cls, file):
+        file_data = cls._read_file(file)
+        number_of_instances = cls._get_number_of_instances(file_data)
+        instance_name = file_data[1].strip()
+        bin_cap, n_itens, best_sol = cls._get_instance_definition(file_data[2])
+        
+        objects = []
+        for line in file_data[3:]:
+            objects.append(int(line))
+            
+        return Instance(instance_name, bin_cap, n_itens, objects, best_sol)
+    
+    @classmethod
+    def _read_file(cls, file):
+        """Reads a file, returning a list with its contents"""
+        input_file = open(file, 'r')
+        data = list(input_file)
+        input_file.close()
+        return data
+
+    
+class Instance(object):
+    def __init__(self, instance_name, bin_cap, n_itens, objects, best_sol):
+        self.instance_name = instance_name
+        self.bin_capacity = bin_cap
+        self.n_itens = n_itens
+        self.objects = objects
+        self.best_known_sol = best_sol
--- tests/test_binp.py
+++ tests/test_binp.py
@@ -1,18 +1,41 @@
 import unittest
+import os
 from binp import *
+
+TEST_DIR = os.path.join(os.curdir, "tests")
+DATA_DIR = os.path.join(TEST_DIR, "data")
 
 class ORLibraryInstanceReaderTest(unittest.TestCase):
 
-	def test_get_instance_definition_with_non_numeric_data_should_raise_valueerror(self):
-		with self.assertRaises(ValueError):
-			ORLibraryInstanceReader._get_instance_definition("10 AA 20")
+    def test_get_instance_definition_with_non_numeric_data_should_raise_valueerror(self):
+        with self.assertRaises(ValueError):
+            ORLibraryInstanceReader._get_instance_definition("10 AA 20")
 
-	def test_get_instance_definition_with_invalid_data_should_raise_valueerror(self):
-		with self.assertRaises(ValueError):
-			ORLibraryInstanceReader._get_instance_definition("invalid line")
+    def test_get_instance_definition_with_invalid_data_should_raise_valueerror(self):
+        with self.assertRaises(ValueError):
+            ORLibraryInstanceReader._get_instance_definition("invalid line")
 
-	def test_get_instance_definition_with_ivalid_data_should_return_1_10_20(self):
-		cap, num, best = ORLibraryInstanceReader._get_instance_definition("1 10 20")
-		self.assertEquals(1, cap)
-		self.assertEquals(10, num)
-		self.assertEquals(20, best)
+    def test_get_instance_definition_with_ivalid_data_should_return_1_10_20(self):
+        cap, num, best = ORLibraryInstanceReader._get_instance_definition("1 10 20")
+        self.assertEqual(1, cap)
+        self.assertEqual(10, num)
+        self.assertEqual(20, best)
+
+    def test_get_number_of_instances_with_valid_data_should_return_4(self):
+        data = ["20", "1 2 3", "22"]
+        ret = ORLibraryInstanceReader._get_number_of_instances(data)
+        self.assertEqual(20, ret)
+        
+    def test_get_number_of_instances_with_invalid_data_should_raise_valueerror(self):
+        data = ["A", "1 2 3", "22"]
+        with self.assertRaises(ValueError):
+            ORLibraryInstanceReader._get_number_of_instances(data)
+
+    def test_get_instances_with_inst1_should_return_1_instance(self):
+        ret = ORLibraryInstanceReader.get_instances(os.path.join(DATA_DIR, "inst1.txt"))
+        self.assertIsInstance(ret, Instance)
+        self.assertEqual("inst_01", ret.instance_name)
+        self.assertEqual(90, ret.bin_capacity)
+        self.assertEqual(3, ret.n_itens)
+        self.assertEqual(2, ret.best_known_sol)
+        self.assertEqual([42, 69, 30], ret.objects)
